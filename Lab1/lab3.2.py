590bc2e33c76dc8ceae39e50549a545ec62dab73
HampusRude <43470686+HampusRude@users.noreply.github.com>
2019-02-09 15:24:35 +0100

    Add files via upload

--- Lab1/lab3.2.py
+++ Lab1/lab3.2.py
@@ -3,8 +3,8 @@ from math import exp
 '''
 Forward pass 	CHECK
 Backpropagation	CHECK
-Change Weights 	Work in progress
-Generate data
+Change Weights 	CHECK
+Generate data 	WIP
 Algorithm to test results
 
 See comment bellow where to continue
@@ -20,9 +20,11 @@ class neuralNet:
 
 		# Weights between input and hidden layer
 		self.W1 = np.random.randn(inputSize+1, hiddenSize)
+		self.delta_W1 = np.zeros((inputSize+1, hiddenSize))
 
 		# Weights between hidden layer and output layer
 		self.W2 = np.random.randn(hiddenSize+1, outputSize)
+		self.delta_W2 = np.zeros((hiddenSize+1, outputSize))
 	
 	# X is the input vector in each iteration.
 	# Input in the input layer and the calculated activation values in the hidden layer
@@ -40,26 +42,21 @@ class neuralNet:
 		delta_o = np.multiply(output - target, self.transDeriv(output))
 
 		# (3,1) matrix with delta errors in hidden layer
-		delta_h = np.multiply(np.dot(self.W2,delta_o), self.transDeriv(self.h_out))
-		delta_h = delta_h[:self.hiddenSize,:] # Take away the last bias part, cause we don't need it
+		delta_h = np.multiply(np.dot(self.W2,delta_o), self.transDeriv(self.h_out))[:self.hiddenSize,:] # Snap of the last index of vector since we wont need it in future (it's the bias term)
 		return delta_h, delta_o
 
-	def updateWeights(self, delta_h, delta_o, row, delta_w1, delta_w2):
+	def updateWeights(self, delta_h, delta_o, row):
+		# Introduce momentu (alpha) taking previous changes in to account as well to suppress fast variations and allow larger learning rate (eta)
 		alpha = 0.9
-		eta = 0.001
+		eta = 0.01	
 
-		delta_w1 = np.multiply(delta_w1, alpha) - np.multiply((1-alpha), np.dot(delta_h, row.T))
+		# Store the delta_w as a parameter so it is easy accessible for the next round
+		self.delta_W1 = np.multiply(self.delta_W1, alpha) - np.multiply((1-alpha), np.dot(delta_h, row.T))
+		self.delta_W2 = np.multiply(self.delta_W2, alpha) - np.multiply((1-alpha), np.dot(delta_o, self.h_out.T)).T
 
-		delta_w2 = np.multiply(delta_w2, alpha) - np.multiply((1-alpha), np.dot(delta_o, self.h_out.T))
-
-		self.W1 += eta*delta_w1
-		self.W2 += eta*delta_w2
-		'''
-
-		CONTINUE HERE
-
-		'''
-		return
+		# Uppdate the weights
+		self.W1 += self.delta_W1*eta
+		self.W2 += self.delta_W2*eta
 
 	def transFunc(self, x):
 		return 2/(1+np.exp(-x))-1
@@ -68,7 +65,30 @@ class neuralNet:
 		return np.multiply((1+x),(1-x))/2
 
 
+def generate_data():
+	N = 200 # nr of inputs per class
+	D = 2 # Dimensionality
+	K = 2 # nr of classes
+	X = np.zeros((D,N*K)) # Empty input matrix with each column being an input
+	for j in range(2):
+		ix = range(N*j,N*(j+1))
+		r = np.linspace(0.0,1,N)
+		t = np.linspace(j*4,(j+1)*4,N) + np.random.randn(N)*0.2 # theta
+		X[ix] = np.c_[r*np.sin(t), r*np.cos(t)]
+	print(X)
+
+'''
+
+CONTINUE HERE 
+
+
+'''
+
+
+
 if __name__ == '__main__':
+	generate_data()
+
 	neuralnet = neuralNet(2,3,1)
 	row = np.matrix([[-1],[-1],[1]])
 	target = np.matrix([[1]])
