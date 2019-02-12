345c165e0072cf830410d73047a77fa9b3c64b4f
Johan Dahl <johad@kth.se>
2019-02-12 10:22:17 +0100

    Fiddled with the sep-bound function

--- Lab1/lab3_1_2.py
+++ Lab1/lab3_1_2.py
@@ -2,16 +2,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 ## Generates two classes of data
-def generate_data(n = 100, bias=True):
-    mA = [6, 6]
-    mB = [-4, -4]
-    sigmaA = [1, 1]
-    sigmaB = [2, 2]
-    classA = np.random.randn(n, 2)*sigmaA + mA
-    classB = np.random.randn(n, 2)*sigmaB + mB
+def generate_data(n = 100, bias=True, specific=True):
+    if specific:
+        mA = [2, 2]
+        mB = [-2, -2]
+        sigmaA = [2, 2]
+        sigmaB = [2, 2]
+    else:
+        mA = [1, 0.3]
+        mB = [0, -0.1]
+        sigmaA = [0.2, 0.2]
+        sigmaB = [0.3, 0.3]
+
+    classA = np.random.randn(int(0.5*n), 2)*sigmaA + mA
+    classB = np.random.randn(int(0.5*n), 2)*sigmaB + mB
 
     # Create X and T matrices
-    target = np.array([1]*n + [-1]*n)
+    target = np.array([1]*int(n*0.5) + [-1]*int(n*0.5))
     pattern = np.concatenate((classA, classB), axis=0)
     # Adds ones for bias
     if bias:
@@ -27,34 +34,10 @@ def generate_data(n = 100, bias=True):
 def delta_rule_batch(w, x, t, epochs=20, learning_rate=0.0001):
     acc = []
     for i in range(epochs):
+        # delta_w = -learning_rate(w*x - t)*x.T
         delta_w = -learning_rate*(np.dot((np.dot(w, x) - t), x.T))
         w = w + delta_w
         acc.append(accuracy(w, x, t))
-    return w, acc
-
-def delta_rule_seq(w, x, t, epochs=20, learning_rate=0.0001):
-    acc = []
-    x = x.T
-    for i in range(epochs):
-        for j in range(len(t)):
-            delta_w = -learning_rate*(np.dot((np.dot(w, x[j]) -t[j]), x[j].T))
-            w = w + delta_w
-        acc.append(accuracy(w, x.T, t))
-    return w, acc
-
-def perceptron_rule_batch(w, x, t, epochs=20, learning_rate=0.0001):
-    acc = []
-    x = x.T
-    for i in range(epochs):
-        delta_w = 0
-        for j in range(len(x)):
-            prediction = np.dot(w, x[j])  # w*X
-            if not (prediction < 0) == (t[j] < 0):
-                error = t[i] - prediction
-                delta_w += learning_rate * np.dot(error, np.transpose(x[j]))
-
-        w = w + delta_w
-        acc.append(accuracy(w, x.T, t))
     return w, acc
 
 ## Returns ratio of missclassified data points
@@ -111,38 +94,13 @@ def main():
     p, t = generate_data(100)
     w_rand = np.random.randn(3)  # Initializing weights
     d_batch_w, db_acc = delta_rule_batch(w_rand, p, t, epochs=e, learning_rate=l_rate)
-    d_seq_w, ds_acc = delta_rule_seq(w_rand, p, t, epochs=e, learning_rate=l_rate)
-    p_batch_w, pb_acc = perceptron_rule_batch(w_rand, p, t, epochs=e, learning_rate=l_rate)
 
     print("Final accuracy for Delta rule, batch: " + str(accuracy(d_batch_w, p, t)))
-    print("Final accuracy for Delta rule, sequential: " + str(accuracy(d_seq_w, p, t)))
-    print("Final accuracy for Perceptron rule, batch: " + str(accuracy(p_batch_w, p, t)))
     print(d_batch_w)
-    print(d_seq_w)
-    print(p_batch_w)
-    plot_sep_bound(p_batch_w, p, t, title="Separation boundary for Single Perceptron using perceptron rule")
     plot_sep_bound(d_batch_w, p, t, title="Separation boundary for Single Perceptron usning Delta rule")
 
     plt.plot(range(len(db_acc)), db_acc, '-', label="Batch Delta rule learning curve")
-    plt.plot(range(len(ds_acc)), ds_acc, '-', label="Sequential Delta rule learning curve")
-    plt.plot(range(len(pb_acc)), pb_acc, '-', label="Perceptron rule learning curve")
-    title = 'Learning curve depending on rule'
-    plt.title(title)
-    plt.grid()
-    plt.legend()
-    plt.show()
-
-def q3():
-    l_rate = 0.0001
-    e = 20
-
-    p, t = generate_data(100, bias=False)
-    w_rand = np.random.randn(2)  # Initializing weights
-    d_batch_w, db_acc = delta_rule_batch(w_rand, p, t, epochs=e, learning_rate=l_rate)
-    print("Final accuracy for Delta rule, batch: " + str(accuracy(d_batch_w, p, t)))
-    print(d_batch_w)
-    plt.plot(range(len(db_acc)), db_acc, '-', label="Batch Delta rule learning curve")
-    title = 'Learning curve without bias'
+    title = 'Learning curve for non-linear separable data'
     plt.title(title)
     plt.grid()
     plt.legend()
@@ -150,4 +108,3 @@ def q3():
 
 if __name__ == "__main__":
     main()
-    #q3()
