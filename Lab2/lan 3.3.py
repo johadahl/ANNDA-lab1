cf3e4b7ac07edfabeff8f5988d5dce2f77729f70
HampusRude <43470686+HampusRude@users.noreply.github.com>
2019-02-18 17:14:03 +0100

    Add files via upload

--- Lab2/lan 3.3.py
+++ Lab2/lan 3.3.py
@@ -72,12 +72,12 @@ class LeastSquares:
 
 class DeltaRule:
 
-	def __init__(self, train, trainTarget, nodesEV, step = 0.01, maxEpoch = 200):
+	def __init__(self, train, trainTarget, nodesEV, step = 0.01, maxEpoch = 100):
 		self.train = train
 		self.trainTarget = trainTarget
 		self.nodesEV = nodesEV
 		self.nodes = nodesEV.size
-		self.weight = np.ones(self.nodesEV.size)
+		self.weight = np.ones((np.size(nodesEV,0),2))
 		self.step = step
 		self.maxEpoch = maxEpoch
 		self.var = np.ones((np.size(nodesEV,0),2)) #variance
@@ -87,7 +87,7 @@ class DeltaRule:
 
 	def mainLoop(self):
 		while self.epoch < self.maxEpoch:
-			for i in range(self.train.size):
+			for i in range(np.size(self.train,0)):
 				self.weight += self.deltaWeights(i)
 				if self.errorCalc(self.train, self.trainTarget) < self.errTreshold:
 					return
@@ -96,51 +96,47 @@ class DeltaRule:
 
 	def deltaWeights(self, index):
 		activations = self.activation(self.train[index])
-		a = self.trainTarget[index] - np.dot(activations, self.weight) 
+		a = self.trainTarget[index] - np.dot(activations.T, self.weight)
+		a = a[[0],:]
 		b = self.step * a
 		delta_w = b * self.activation(self.train[index])
 		return delta_w
 
 	def activation(self, x):
-		print("X Shape")
-		print(x)
-		print(" ")
-		print(np.shape(self.nodesEV))
-		print("Shape of Var")
-		print(np.shape(self.var))
 		activations = np.exp(np.multiply((-(x-self.nodesEV)**2),((2*self.var)**(-1))))
 
 		#3 vikter ska uppdateras        
 		#1a
-		bestActivationIndex1 = np.argmax(activations)
-		activations[bestActivationIndex1]=0
+		bestActivationIndex1 = np.argmax(activations.sum(axis=1))
+		activations[bestActivationIndex1]= 0
+
 
 		#2a
-		bestActivationIndex2 = np.argmax(activations)
+		bestActivationIndex2 = np.argmax(activations.sum(axis=1))
 		activations[bestActivationIndex2]=0
 
 		#3a
-		bestActivationIndex3 = np.argmax(activations)
+		bestActivationIndex3 = np.argmax(activations.sum(axis=1))
 		activations[bestActivationIndex3]=0
 
 		#4e
-		bestActivationIndex4 = np.argmax(activations)
+		bestActivationIndex4 = np.argmax(activations.sum(axis=1))
 		activations[bestActivationIndex4]=0
 
 		#5e
-		bestActivationIndex5 = np.argmax(activations)
+		bestActivationIndex5 = np.argmax(activations.sum(axis=1))
 		activations[bestActivationIndex5]=0
 
 		activations = np.zeros_like(activations)
 		activations[bestActivationIndex1]=1
-		activations[bestActivationIndex2]=1
-		activations[bestActivationIndex3]=1
+		activations[bestActivationIndex2]=0.8
+		activations[bestActivationIndex3]=0.6
 		activations[bestActivationIndex4]=0.4
 		activations[bestActivationIndex5]=0.2
 		return activations
 
 	def run(self, input):
-		result = np.zeros(input.size)
+		result = np.zeros((np.size(input,0),2))
 		c = 0
 		for x in input:
 			result[c] = sum(np.multiply(self.weight, self.activation(x)))
@@ -158,35 +154,38 @@ if __name__ == '__main__':
 	ballist = np.loadtxt('ballist.dat')
 	balltest = np.loadtxt('balltest.dat')
 
+	# Training data
 	angleVelocity = ballist[:,[0,1]]
-	print(np.shape(angleVelocity))
 	distHeight = ballist[:,[2,3]]
+
+	# Testing data
+	angleVelocityTest = balltest[:,[0,1]]
+	distHeightTest = balltest[:,[2,3]]
 
 
 	index = np.linspace(0, (angleVelocity.size-1)/2, 10, dtype = int)
 	b = DeltaRule(angleVelocity, distHeight, angleVelocity[index], 0.01, 100)
-
+	'''
 	plt.plot(angleVelocity, b.run(angleVelocity), 'green')
 	plt.plot(angleVelocity, trainTSin, 'red')
 	plt.plot(angleVelocity[index], distHeight[index], 'b+')
 	plt.title("Training data")
 	plt.show()
-
+	'''
 	testError = []
 	trainError = []
 
-	print("Residual error is: ", b.errorCalc(trainX, trainTSin))
+	print("Residual error is: ", b.errorCalc(angleVelocityTest, distHeightTest))
+	
 	print("Nodes \t Training error \t Testing error") 
-	for i in range(1, 26):
-		index = np.linspace(0, trainX.size-1, num = i, dtype = int)
-		dr = DeltaRule(trainX, trainTSin, trainX[index], 0.01, 100)
-		print(i, " \t ", dr.errorCalc(trainX, trainTSin), " \t ", dr.errorCalc(testX, testTSin))
-		testError.append(dr.errorCalc(testX, testTSin))
-		trainError.append(dr.errorCalc(trainX, trainTSin))
+	for i in range(1, 25):
+		index = np.linspace(0, (angleVelocity.size-1)/2, num = i, dtype = int)
+		dr = DeltaRule(angleVelocity, distHeight, angleVelocity[index], 0.01, 100)
+		print(i, " \t ", dr.errorCalc(angleVelocity, distHeight), " \t ", dr.errorCalc(angleVelocityTest, distHeightTest))
+		testError.append(dr.errorCalc(angleVelocityTest, distHeightTest))
+		trainError.append(dr.errorCalc(angleVelocity, distHeight))
 
 	plt.plot(range(len(testError)), testError, label = "test error")
 	plt.plot(range(len(testError)), trainError, label = "training error")
 	plt.legend()
 	plt.show()
-
-
