1bd744d322b5caaba0bb8a186a594877ae4228e5
Viktor Paulsrud <paulsrud@kth.se>
2019-02-19 10:57:26 +0100

    added lab3

---
+++ Lab3/lab 3 3.1.py
@@ -0,0 +1,26 @@
+import numpy as np
+import random, math
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from scipy import signal
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+np.random.seed(42)
+
+x1 = np.array([-1, -1, 1, -1, 1, -1, -1, 1])
+x2 = np.array([-1, -1, -1, -1, -1, 1, -1, -1])
+x3 = np.array([-1, 1, 1, -1, -1, 1, -1, 1])
+
+
+
+
+if __name__ == '__main__':
+	pass
+
+	"""
+
+	plt.plot(range(len(testError)), testError, label = "test error")
+	plt.plot(range(len(testError)), trainError, label = "training error")
+	plt.legend()
+	plt.show()"""
+
